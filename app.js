bffc03768099921adf50dbf67393876bd272800b
Robert Drabant <rdrabant@resultsdelivered.com>
2021-12-06 21:21:09 -0800

    initial upload

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const myMap = {
 	buildMap() {
 		this.map = L.map('map', {
 		center: this.coordinates,
-		zoom: 11,
+		zoom: 10,
 		});
 		// add openstreetmap tiles
 		L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -51,15 +51,21 @@ async function getFoursquare(business) {
 	const options = {
 		method: 'GET',
 		headers: {
-		Accept: 'application/json',
-		Authorization: 'fsq3ATzZbmcGhdeFafr73wZcnJ+LlN6bK+4dh19a7ClS4u8='
-		}
+			Accept: 'application/json',
+			Authorization: 'insert your api key here'
+		},
+		mode: "cors" 
 	}
 	let limit = 5
 	let lat = myMap.coordinates[0]
 	let lon = myMap.coordinates[1]
-	let response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`, options)
+	let url = `https://cors-anywhere.herokuapp.com/https://api.foursquare.com/v3/places/search?&query=${business}&limit=${limit}&ll=${lat}%2C${lon}`;
+	console.log("URL: " + url);
+	console.log("options object: " + JSON.stringify(options));
+	let response = await fetch(url, options);
+	
 	let data = await response.text()
+	console.log(data);
 	let parsedData = JSON.parse(data)
 	let businesses = parsedData.results
 	return businesses
@@ -91,6 +97,7 @@ document.getElementById('submit').addEventListener('click', async (event) => {
 	event.preventDefault()
 	let business = document.getElementById('business').value
 	let data = await getFoursquare(business)
+	console.log('businesses: ' + business);
 	myMap.businesses = processBusinesses(data)
 	myMap.addMarkers()
 })
